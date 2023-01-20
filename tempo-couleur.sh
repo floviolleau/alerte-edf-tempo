da6fc7975d0bf0b0c12be34e0141e419c1974a36
floviolleau <floviolleau@users.noreply.github.com>
2023-01-20 22:17:50 +0100

    🐛 Fix wrong color applied

--- tempo-couleur.sh
+++ tempo-couleur.sh
@@ -11,26 +11,31 @@ adresse="https://${adresse}${DATEJOUR}"
 RESULT="`wget -qO- $adresse`"
 RESULT_JOURS_RESTANTS="`wget -qO- $adresseJoursRestants`"
 
+COLOR_GREY="\U0002B1B"
+COLOR_RED="\U0001F7E5"
+COLOR_WHITE="\U0002B1C"
+COLOR_BLUE="\U0001F7E6"
+
 coul_J=$(echo $RESULT | jq -r '.couleurJourJ')
 coul_J1=$(echo $RESULT | jq -r '.couleurJourJ1')
 
-if [[ "$coul_J" == 'TEMPO_BLEU' || "$coul_J1" == 'TEMPO_BLEU' ]]; then
-    picto_coul_j="\U0001F7E6"
-    picto_coul_j1="\U0001F7E6"
-elif [[ "$coul_J" == 'TEMPO_BLANC' || "$coul_J1" == 'TEMPO_BLANC' ]]; then
-    picto_coul_j="\U0002B1C"
-    picto_coul_j1="\U0002B1C"
-elif [[ "$coul_J" == 'TEMPO_ROUGE' || "$coul_J1" == 'TEMPO_ROUGE' ]]; then
-    picto_coul_j="\U0001F7E5"
-    picto_coul_j1="\U0001F7E5"
-else
-    picto_coul_j="\U0002B1B"
-    picto_coul_j1="\U0002B1B"
-fi
+color () {
+    if [[ "$1" == 'TEMPO_BLEU' ]]; then
+        echo $COLOR_BLUE
+    elif [[ "$1" == 'TEMPO_BLANC' ]]; then
+        echo $COLOR_WHITE
+    elif [[ "$1" == 'TEMPO_ROUGE' ]]; then
+        echo $COLOR_RED
+    else
+        echo $COLOR_GREY
+    fi
+}
+
+picto_coul_j=$(color $coul_J)
+picto_coul_j1=$(color $coul_J1)
 
 coul_ROUGE=$(echo $RESULT_JOURS_RESTANTS | jq -r '.PARAM_NB_J_ROUGE')
 coul_BLANC=$(echo $RESULT_JOURS_RESTANTS | jq -r '.PARAM_NB_J_BLANC')
 coul_BLEU=$(echo $RESULT_JOURS_RESTANTS | jq -r '.PARAM_NB_J_BLEU')
-
+printf "%b" "Jours restants : " "$COLOR_RED" " $coul_ROUGE jour(s) rouge(s), " "$COLOR_WHITE" " $coul_BLANC jour(s) blanc(s) et " "$COLOR_BLUE" " $coul_BLEU jour(s) bleu(s)"
 printf "%b" "$DATEJOUR_FR : " "$picto_coul_j" " $coul_J, Demain $DATEDEMAIN_FR : " "$picto_coul_j1" " $coul_J1"
-printf "%b" "Jours restants : " "\U0001F7E5" " $coul_ROUGE jour(s) rouge(s), " "\U0002B1C" " $coul_BLANC jour(s) blanc(s) et " "\U0001F7E6" " $coul_BLEU jour(s) bleu(s)"
